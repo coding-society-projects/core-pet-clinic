776b4dda5e0cc343a7412791a0b0721ffb6e45d6
gprok <gprok60@gmail.com>
2021-02-20 19:03:36 +0200

    DB initializer added to create roles

---
+++ Data/DBInitializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PetClinic.Data
+{
+    public static class DBInitializer
+    {
+        public static async Task Initialize(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider
+                .GetRequiredService<ApplicationDbContext>();
+            context.Database.EnsureCreated();
+            var roleManager = serviceProvider
+                .GetRequiredService<RoleManager<IdentityRole>>();
+
+            string[] RoleNames = { "Admin", "Owner", "Vet" };
+
+            foreach (string roleName in RoleNames)
+            {
+                IdentityResult result;
+                var roleExist = await roleManager.RoleExistsAsync(roleName);
+                if (!roleExist)
+                {
+                    result = await roleManager
+                        .CreateAsync(new IdentityRole(roleName));
+                }
+            }
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace PetClinic.Models
+{
+    public class Person : IdentityUser 
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+    }
+}
